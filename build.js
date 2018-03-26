b182488d2c2e4d34b920cc28620530a3e94aaa0c
Andor Kovács <akovacs@ovitas.hu>
2018-03-26 16:44:16 +0200

    Updated build process

---
+++ build.js
@@ -0,0 +1,29 @@
+const fs = require('fs-extra')
+
+fs.remove('production', removeProductionCallback)
+
+function removeProductionCallback() {
+	callback('Removed production directory.')()
+	fs.copy('source', 'production/bt9fs3', copySourceCallback)
+}
+
+function copySourceCallback() {
+	callback('Copied source to production directory.')()
+	fs.remove('production/bt9fs3/scss', removeSassCallback)
+}
+
+function removeSassCallback() {
+	callback('Removed scss directory.')()
+	fs.move('production/bt9fs3/project', 'production', callback(`Moved project directory.`))
+	fs.move('production/bt9fs3/php/page', 'production/bt9fs3', callback(`Moved page directory.`))
+	fs.move('production/bt9fs3/php/index.php', 'production/bt9fs3/index.php', callback(`Moved index.php file.`))
+}
+
+function callback(task) {
+	return error => {
+		if (error) {
+			throw new Error(error)
+		}
+		console.log(task)
+	}
+}
